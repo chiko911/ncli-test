11b9dbfd0ebfc6b0f57c412c093e45d3e8434b40
chiko911 <71180624+chiko911@users.noreply.github.com>
2022-02-23 11:47:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y xfce4 xfce4-goodies
-sudo apt install tightvncserver
-vncserver
-cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
 npm install -g near-cli
 export NEAR_ENV=mainnet 
 sudo apt install -y firefox
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+sudo apt install tightvncserver
+vncserver
+cd ~
+mv $HOME/.vnc/xstartup ~/.vnc/xstartup.bak
 bash -c "cat > $HOME/.vnc/xstartup<<EOF
 xrdb $HOME/.Xresources
 startxfce4 &
