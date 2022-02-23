5dfe87f24082780bee5fe51e908233e6bbc000ae
chiko911 <71180624+chiko911@users.noreply.github.com>
2022-02-23 13:18:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y xfce4 xfce4-goodies
+sudo apt-get install autocutsel
 curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt install nodejs
@@ -14,6 +15,7 @@ mv $HOME/.vnc/xstartup ~/.vnc/xstartup.bak
 bash -c "cat > $HOME/.vnc/xstartup<<EOF
 xrdb $HOME/.Xresources
 startxfce4 &
+autocutsel -fork
 EOF"
 chmod +x ~/.vnc/xstartup
 vncserver -kill :1
